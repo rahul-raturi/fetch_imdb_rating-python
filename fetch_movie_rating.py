5c529ae47cdca17a9a602d199be8f883584f25c0
rahul raturi <rhlrtr44@gmail.com>
2015-09-25 19:16:51 +0530

    Included year in results. Easier to identify movies.

--- fetch_movie_rating.py
+++ fetch_movie_rating.py
@@ -6,36 +6,41 @@ from os import system, listdir
 
 
 def fetch_rating(movie):
+	movie = '+'.join(movie.split())
 	search_url = "http://omdbapi.com/?t="+movie+"&type=movie"
-	print search_url
 	movie_data = requests.get(search_url).json()
 	if movie_data['Response'] == "False":
 		return ' ', ' '
-	return movie_data['Title'], movie_data['imdbRating']
+	return movie_data['Title'], movie_data['imdbRating'], movie_data['Year']
 
 
 def gather_results(movies_list):
 	movies = list()
-	for movie in movies_list:
+	print "Wait, fetching data."
+	total = len(movies_list)
+	for i, movie in enumerate(movies_list):
+		print '* '+str(i)+'/'+str(total)+' --> '+movie
 		tmp = fetch_rating(movie)
-		if tmp[0] == ' ':
-			tmp[0] = movie
-			tmp[1] = 'N/A'
-		movies.append(tmp)
+		if tmp[0] != ' ':
+			movies.append(tmp)
 	return movies
 
 def display_results(movies):
-	system('clear')
-	top_bar = '{0: ^50}'.format('Title') + '|' + '{0: ^9}'.format('Rating')
+	print '\n\n'+' '*25+'***RESULTS***'
+	print ' '*25+'-------------\n'
+	top_bar = '{0: ^50}'.format('Title') + '|' + '{0: ^10}'.format('Year') + '|' +  '{0: ^10}'.format('Rating')
 	print top_bar
 	print '-'*len(top_bar)
 
 	for movie in movies:
+		title = movie[0].encode('ascii', 'ignore')
+		rating = movie[1]
+		year = movie[2]
 		if(len(movie[0]) > 50):
 			title = title[0:47] + '...'
-		print '{0: <50}'.format(movie[0])+'|'+'{0: ^9}'.format(movie[1])
+		print '{0: <50}'.format(title)+'|'+'{0: ^10}'.format(year)+'|'+'{0: ^10}'.format(rating)
 
-	print '-'*60
+	print '-'*72
 
 
 def is_year_or_reso(wrd):
@@ -54,8 +59,10 @@ def format_movie_names(files_list):
 	movies = list()
 
 	for name in files_list:
-		name = name.encode('utf-8', 'ignore')
-		if name != None:
+		name = name.encode('ascii', 'ignore')
+		fmt = name.rsplit('.', 2)
+		not_srt = len(fmt) > 1 and fmt[1] != 'srt'
+		if name != None and not not_srt:
 			tmp = (name.split('('))[0]
 			if (tmp.split('.'))[-1] in ['mp4', 'mkv', 'avi']:
 				tmp = ' '.join((tmp.split('.'))[0:-1])
@@ -82,16 +89,20 @@ def main():
 	print '1 -> Enter movie title'
 	print '2 -> Search a local directory for movies'
 	choice = raw_input(': ')
+
 	if choice == '1':
 		movies.append('+'.join(raw_input("Enter movie name: ").split()))
-		display_results(movies)
+		res = gather_results(movies)
+		display_results(res)
+
 	elif choice == '2':
 		while True:
 			direc = raw_input("Enter directory: ")
 			try:
 				ls = listdir(direc)
 				movies.extend(format_movie_names(ls))
-				display_results(movies)
+				res = gather_results(movies)
+				display_results(res)
 				break
 
 			except OSError:
